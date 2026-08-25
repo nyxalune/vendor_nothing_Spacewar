b679c983167275a22f14e99ad32f427b2ea959f2
AdarshGrewal <adarshgrewal@gmail.com>
2026-08-25 13:54:45 +0530

    Spacewar: Update system WFD stack from marvel_g A171WEH.20
    Change-Id: I055caf09667d51e9ad99022eeb892c2c6de7fce0

--- Spacewar-vendor.mk
+++ Spacewar-vendor.mk
@@ -1,3 +1,5 @@
+
+
 #
 # Automatically generated file. DO NOT MODIFY
 #
@@ -1156,7 +1158,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.dpmservice@1.0 \
     vendor.qti.hardware.dpmservice@1.1 \
     vendor.qti.hardware.wifidisplaysession@1.0 \
-    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
+    vendor.qti.hardware.wifidisplaysession_aidl-V2-ndk \
     vendor.qti.imsrtpservice@3.0 \
     vendor.qti.imsrtpservice@3.1 \
     CACertService \
